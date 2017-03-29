25899fcd9c657b622b4c9e0d57748de8aef4a4a6
Willem Goudsbloem <willem.goudsbloem@gmail.com>
2017-03-29 13:17:54 -0400

    put and get

--- store/store.go
+++ store/store.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"bytes"
 	"io"
 )
 
@@ -8,24 +9,74 @@ type Storer interface {
 	Put(p []byte) (offset int64, err error)
 }
 
+type ReadAtWriteSeeker interface {
+	io.ReadWriteSeeker
+	io.ReaderAt
+}
+
 type store struct {
-	ws  io.WriteSeeker
-	end int64
+	stream ReadAtWriteSeeker
+	end    int64
 }
 
-func NewStorer(ws io.WriteSeeker) Storer {
-	e, err := ws.Seek(0, io.SeekEnd)
-	if err != nil {
-		panic(err)
-	}
-	return store{ws, e}
-}
+// func NewStorer(ws io.WriteSeeker) Storer {
+// 	e, err := ws.Seek(0, io.SeekEnd)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	return store{ws, e}
+// }
 
-func (s store) Put(p []byte) (offset int64, err error) {
-	n, err := s.ws.Write(p)
+func (s *store) Put(p []byte) (offset int64, err error) {
+	p = append(p, '\n')
+	n, err := s.stream.Write(p)
 	if err != nil {
 		return
 	}
-	offset = s.end + int64(n)
+	s.end += int64(n)
+	offset = s.end
+	return
+}
+
+func (s *store) Get() (p []byte, err error) {
+	b := make([]byte, 1)
+	var bb bytes.Buffer
+	for {
+		_, err = s.stream.Read(b)
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+				break
+			}
+			return
+		}
+		if b[0] != '\n' {
+			_, err = bb.Write(b)
+			if err != nil {
+				return
+			}
+		} else {
+			break
+		}
+	}
+	p = bb.Bytes()
+	return
+}
+
+var buf int64 = 500
+
+func lastEntry(sample []byte) (result []byte) {
+	var start, end int
+	for n := len(sample) - 1; n > -1; n-- {
+		if sample[n] == '\n' {
+			if end == 0 {
+				end = n
+			} else {
+				start = n + 1
+				break
+			}
+		}
+	}
+	result = sample[start:end]
 	return
 }
--- store/store_test.go
+++ store/store_test.go
@@ -13,12 +13,22 @@ func (t *TestSeeker) Seek(offset int64, whence int) (int64, error) {
 	return int64(t.Len()), nil
 }
 
+func (t *TestSeeker) ReadAt(p []byte, off int64) (n int, err error) {
+	return t.Read(p)
+}
+
 // store a string into a buffer
 func TestPut(t *testing.T) {
-	exp := []byte("teststring")
+	in1 := "teststring"
+	in2 := "teststring2"
+	exp := in1 + "\n" + in2 + "\n"
 	var bb TestSeeker
 	s := store{&bb, 0}
-	offset, err := s.Put(exp)
+	_, err := s.Put([]byte(in1))
+	if err != nil {
+		t.Error(err)
+	}
+	offset, err := s.Put([]byte(in2))
 	if err != nil {
 		t.Error(err)
 	}
@@ -26,8 +36,57 @@ func TestPut(t *testing.T) {
 	if offset != expLen {
 		t.Errorf("want %v, got %v", expLen, offset)
 	}
-	res, _ := bb.ReadBytes('\t')
+	res, _ := bb.ReadString('\t')
 	if exp != res {
 		t.Errorf("want %v, got %v", exp, res)
+	}
+}
+
+func TestGet(t *testing.T) {
+	in1 := "teststring"
+	in2 := "teststring2"
+	var bb TestSeeker
+	s := store{&bb, 0}
+	_, err := s.Put([]byte(in1))
+	if err != nil {
+		t.Error(err)
+	}
+	offset, err := s.Put([]byte(in2))
+	if err != nil {
+		t.Error(err)
+	}
+	b, err := s.Get()
+	if err != nil {
+		t.Error(err)
+	}
+	if string(b) != in1 {
+		t.Errorf("want %v got %v", in1, string(b))
+	}
+	if string(b) == in2 {
+		t.Errorf("want %v got %v", in1, string(b))
+	}
+	s2 := store{&bb, offset}
+	b2, err := s2.Get()
+	if err != nil {
+		t.Error(err)
+	}
+	if string(b2) != in2 {
+		t.Errorf("want [%v] got [%v]", in2, string(b2))
+	}
+}
+
+func TestInternalLastEntry(t *testing.T) {
+	expVal1 := `{"test1":"val1"}`
+	expVal2 := `{"test2":"val2"}`
+	testVal1 := expVal1 + "\n"
+	testVal2 := expVal2 + "\n"
+	testVal3 := testVal1 + testVal2
+	res1 := lastEntry([]byte(testVal1))
+	if string(res1) != expVal1 {
+		t.Errorf("want: '%v' got: '%v'", expVal1, string(res1))
+	}
+	res2 := lastEntry([]byte(testVal3))
+	if string(res2) != expVal2 {
+		t.Errorf("want: '%v' got: '%v'", expVal2, string(res2))
 	}
 }
